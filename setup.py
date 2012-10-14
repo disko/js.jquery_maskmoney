5c68432f06d22c2ce68ff73f317be9e7aecbfc97
Andreas kaiser <disko@binary-punks.com>
2012-10-14 15:29:09 +0200

    Bump version.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ import os
 # For example, a packaging bugfix release version 1.4.4 of the
 # js.jquery package would be version 1.4.4-1 .
 
-version = '1.4.1dev'
+version = '1.4.1'
 
 
 def read(*rnames):

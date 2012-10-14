725470edd3d192d35f671c1ccbd7a9bffbb927cb
Andreas kaiser <disko@binary-punks.com>
2012-10-14 15:38:38 +0200

    Add url.

--- setup.py
+++ setup.py
@@ -34,6 +34,7 @@ setup(
     keywords='',
     author='Andreas Kaiser',
     author_email='disko@binary-punks.com',
+    url='https://github.com/disko/js.jquery_maskmoney',
     license='BSD',
     packages=find_packages(),
     namespace_packages=['js'],
